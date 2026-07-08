7122e4d3cfb350dea132913bd625e363dbf918ae
Kaylee Ervin <kaylee.ervin1510@gmail.com>
2026-07-08 12:10:09 -0700

    first commit for contact list, just getting things read

--- contact_list.py
+++ contact_list.py
@@ -1,2 +1,10 @@
 class ContactList:
 
+
+    def contact_info(self, first_name, last_name, number, image):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.number = number
+        self.image = image
+    
+    def add_contact({})
